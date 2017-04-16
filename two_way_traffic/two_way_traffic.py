304045c9a877619341ee3680f01a5cc29d39a836
Ankit Dutta <geekankit318@gmail.com>
2017-04-16 19:29:52 +0530

    Revert "Revert "Final submit for version 1.0""
    This reverts commit f88db03f1d84166eaf9cc0ee5fd8a7b957eb908b.

--- two_way_traffic/two_way_traffic.py
+++ two_way_traffic/two_way_traffic.py
@@ -1,3 +1,4 @@
+# import libraries
 import sys
 import numpy
 import cv2
@@ -5,6 +6,8 @@ import serial
 import time
 
 cascade_src = 'dataset\cars.xml'
+# Create Serial port object called arduinoSerialData
+ser = serial.Serial('COM3', 9600, timeout = 1)
 
 print("Starting app.")
 
@@ -25,10 +28,10 @@ for i in img_src:
         sys.exit("Error: Input not in image format")
 
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    print("Filtering image to remove noises")
-    dst = cv2.fastNlMeansDenoising(gray,None,3,21,7)
+    #print("Filtering image to remove noises")
+    #dst = cv2.fastNlMeansDenoising(gray,None,3,21,7)
 
-    cars = car_cascade.detectMultiScale(gray, 1.08, 3)
+    cars = car_cascade.detectMultiScale(gray, 1.05, 3)
 
     print("Proceeing...")
     for (x,y,w,h) in cars:
@@ -36,10 +39,15 @@ for i in img_src:
         no_of_cars = no_of_cars + 1
     
     print(no_of_cars)
+    time_delay = int(2 + (0.5*no_of_cars))
+    
+    ser.write('%d' %time_delay)
+    time.sleep(time_delay)
     while cv2.waitKey(33) != 27:
         cv2.namedWindow('Output',cv2.WINDOW_NORMAL)
         cv2.resizeWindow('Output', 1000,700)
         cv2.imshow('Output', img)
 
     cv2.destroyAllWindows()
+
 
